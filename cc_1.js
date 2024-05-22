b6fc914e33ab12e0f9f396eb953e12274906c1e0
anjalikrishna-11 <123520060+anjalikrishna-11@users.noreply.github.com>
2024-05-22 11:07:59 -0400

    Add output functionality to display bill, tip, and total.

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,8 @@
-// Step 1: Determine the tip amount based on the bill using a ternary operator
+//Anjali Krishna (U35346496)
+//Coding Challenge 2: Tip Calculator
+// Determine the tip amount based on the bill using a ternary operator
 const bill = 275;
 const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// Display a message in the console that includes the bill amount, the tip amount, and the total cost
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}`);
+
