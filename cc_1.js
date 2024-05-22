0ab0fc520b8416b3cd98c8951362dd5d6ff4f002
anjalikrishna-11 <123520060+anjalikrishna-11@users.noreply.github.com>
2024-05-22 11:10:25 -0400

    Implement and populate arrays for bills, tips, and totals.

--- cc_1.js
+++ cc_1.js
@@ -13,3 +13,35 @@ function calcTip(bill) {
 const testBill = 100;
 const testTip = calcTip(testBill);
 console.log(`Test bill: $${testBill}, Test tip: $${testTip}`);
+
+//Construct an array bills containing the test data values for data set 1
+const bills1 = [275, 40, 430];
+// Generate an array tips where each entry is the result of calling calcTip for each corresponding bill value
+const tips1 = [];
+const totals1 = [];
+
+for (let i = 0; i < bills1.length; i++) {
+    tips1[i] = calcTip(bills1[i]);
+    totals1[i] = bills1[i] + tips1[i];
+}
+
+// Display the arrays
+console.log('Data Set 1')
+console.log('Bills:', bills1);
+console.log('Tips:', tips1);
+console.log('Totals:', totals1);
+
+//Repeat the same for data set 2
+const bills2 = [125, 555, 44];
+const tips2 = [];
+const totals2 = [];
+
+for (let i = 0; i < bills2.length; i++) {
+    tips2[i] = calcTip(bills2[i]);
+    totals2[i] = bills2[i] + tips2[i];
+}
+
+console.log('Data Set 2')
+console.log('Bills:', bills2);
+console.log('Tips:', tips2);
+console.log('Totals:', totals2);
